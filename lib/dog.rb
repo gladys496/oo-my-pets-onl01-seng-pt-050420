06bc25e6ffe03c02c0cf52de80cf53721d76fe1d
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-20 19:15:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,14 @@ class Dog
  attr_accessor :owner, :mood 
  attr_reader :name 
  
+ 
+ @@all = []
+ 
  def initialize(name, owner)
    @name = name 
    @owner = owner 
    @mood = "nervous"
+   @@all << self 
  end 
  
  
