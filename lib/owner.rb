75f5911ba2a398c7955cbcca7e95f2f6cf6aa361
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-20 19:33:56 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -52,10 +52,11 @@ def feed_cats
   self.cats.each {|cat| cat.mood = "happy"}
 end 
 
-def sell_pets 
-  self.all.select.clear {|pet| pet.mood = "nervous"}
-  
+ def sell_pets
+    self.cats.each do |cat|
+      self.cats.delete(cat)
+      cat.owner = nil
+      cat.mood = "nervous"
 end 
-
 
 end
