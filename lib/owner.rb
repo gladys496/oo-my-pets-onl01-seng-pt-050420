d93db7a1a6a028e7734bfe7a54e4b9b8493312e2
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-20 19:13:22 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species 
+  
+  @@all = []
+  
+  def initialize(name, species = "human")
+    @name = name
+    @species = species 
+    @@all << self 
+  end 
+def say_species
+        "I am a #{self.species}."
+  end 
+  
+  def self.all
+      @@all 
+    end 
+    
+    def self.count 
+      self.all.length 
+    end 
+  
+  def self.reset_all 
+    self.all.clear 
+  end 
+  
+
 end
